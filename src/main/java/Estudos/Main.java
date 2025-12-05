e39d0dac91b9807d37ce7e881dd0597fa99e389d
Rafael_Andrade <r-andrade-siqueira@hotmail.com>
2025-12-05 20:33:32 -0300

    Continuando a lidar com arquivos

--- src/main/java/Estudos/Main.java
+++ src/main/java/Estudos/Main.java
@@ -1,9 +1,13 @@
 package Estudos;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Pessoa> pessoas = new ArrayList<>();
 
         try{
             InputStream is = new FileInputStream("src/main/java/Estudos/cadastro.csv");
@@ -11,9 +15,32 @@ public class Main {
             BufferedReader br = new BufferedReader(isr);
 
             String linha;
+            boolean primeiraLinha = true;
 
             while ((linha = br.readLine()) != null) {
-                System.out.println(linha);
+
+                if (primeiraLinha) {
+                    primeiraLinha = false;
+                    continue;
+                }
+
+                String[] partes = linha.split(",");
+
+                String nome = partes[0];
+                String cargo = partes[1];
+                char sexo = partes[2].charAt(0);
+                Integer idade = Integer.parseInt(partes[3]);
+
+                Pessoa pessoa = new Pessoa(
+                        partes[0],
+                        partes[1],
+                        partes[2].charAt(0),
+                        Integer.parseInt(partes[3])
+                );
+
+                pessoas.add(pessoa);
+
+                //System.out.println(linha);
             }
 
             br.close();
@@ -23,6 +50,11 @@ public class Main {
         } catch (Exception e){
             System.out.println("Erro no arquivo!");
             System.out.println(e.getMessage());
+        }
+
+        System.out.println("\n--- PESSOAS CADASTRADAS ---");
+        for (Pessoa p : pessoas) {
+            p.apresentar();
         }
 
     }
---
+++ src/main/java/Estudos/Pessoa.java
@@ -0,0 +1,23 @@
+package Estudos;
+
+public class Pessoa {
+    String nome;
+    String cargo;
+    char sexo;
+    Integer idade;
+
+
+    public Pessoa(String nome, String cargo, char sexo, Integer idade){
+
+
+        this.nome = nome;
+        this.cargo = cargo;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+
+    public void apresentar(){
+        System.out.println("Ola, sou o " + this.nome + ", meu cargo é " + this.cargo + ", meu sexo é "
+        + this.sexo + " e minha idade é " + this.idade);
+    }
+}
